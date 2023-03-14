43e91617f47d37078a822c75069b85af53757c85
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-03-13 21:54:25 -0300

    Refactor: changes improvement App02

--- s03Exe/JAVA-COLLECTIONS/src/App02.java
+++ s03Exe/JAVA-COLLECTIONS/src/App02.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+// import java.util.HashSet;
+// import java.util.Iterator;
+// import java.util.List;
 import java.util.Map;
-import java.util.Set;
+// import java.util.Set;
 
 public class App02 {
     public static void main(String[] args) throws Exception {
