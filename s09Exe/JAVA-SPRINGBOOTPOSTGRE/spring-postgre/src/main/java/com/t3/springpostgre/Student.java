f324234673565c432c437f22672cbeed7db3b90e
unknown <elisandro.santolin@outlook.com>
2023-04-18 22:23:56 -0300

    Feat: implementation pagination more method delete

--- s09Exe/JAVA-SPRINGBOOTPOSTGRE/spring-postgre/src/main/java/com/t3/springpostgre/Student.java
+++ s09Exe/JAVA-SPRINGBOOTPOSTGRE/spring-postgre/src/main/java/com/t3/springpostgre/Student.java
@@ -15,19 +15,21 @@ public class Student {
     private String firstName;
     private String lastName;
     private String course;
-    
+    private Period period; // ## isso é um ENUM ## //
+
     public Student() { // ## necessário para o JPA trabalhar com o banco ## //
     }
 
-    public Student(String firstName, String lastName, String course) { // ## não gera o RA pois ele é gerado automáticamente ## //
+    public Student(String firstName, String lastName, String course, Period period) { // ## não gera o RA pois ele é gerado automáticamente ## //
         this.firstName = firstName;
         this.lastName = lastName;
         this.course = course;
+        this.period = period;
     }
 
     @Override
     public String toString() {
-        return "Student [ra=" + ra + ", firstName=" + firstName + ", lastName=" + lastName + ", course=" + course + "]";
+        return "Student [ra=" + ra + ", firstName=" + firstName + ", lastName=" + lastName + ", course=" + course + ", period=" + period +"]";
     }
 
     public long getRa() {
@@ -61,5 +63,13 @@ public class Student {
     public void setCourse(String course) {
         this.course = course;
     }
-     
+        
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
 }
