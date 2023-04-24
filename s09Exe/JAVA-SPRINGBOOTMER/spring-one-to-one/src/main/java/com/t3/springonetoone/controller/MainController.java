897e8a44c6e0d1ab29b9056ac5be84e75cd92bff
unknown <elisandro.santolin@outlook.com>
2023-04-24 20:32:49 -0300

    Refactor: adjust templates - not finished

--- s09Exe/JAVA-SPRINGBOOTMER/spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
+++ s09Exe/JAVA-SPRINGBOOTMER/spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
@@ -1,19 +1,26 @@
 package com.t3.springonetoone.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.t3.springonetoone.entity.Person;
 import com.t3.springonetoone.entity.Pet;
 import com.t3.springonetoone.repository.PersonRepository;
+import com.t3.springonetoone.repository.PetRepository;
 
 @Controller
 public class MainController {
     
     @Autowired
     private PersonRepository personRepo;
+
+    @Autowired
+    private PetRepository petRepo;
     
     @GetMapping("/")
     public String index(){
@@ -24,11 +31,30 @@ public class MainController {
     public String createPerson(Model model){
         Person person = new Person(); // cria a pessoa 
         Pet pet = new Pet(); // cria um pet
-        person.setPetId(pet); // incula o pet a pessoa
+        person.setPetId(pet); // inclue o pet a pessoa
         
-
         model.addAttribute("person", person);
         return "addEditPerson";
     }
+   
+    @PostMapping("/persons/save")
+    public String savePerson(Person person){
+        personRepo.save(person); // ## Cascade.ALL salva ambas entidades
+        return "redirect:/persons"; 
+    }
 
+    @GetMapping("/persons")
+    public String getAllPersons(Model model){
+        List<Person> persons = personRepo.findAll();
+        model.addAttribute("persons", persons);
+        return "PersonList";
+    }
+
+    @GetMapping("/pets")
+    public String getAllPets(Model model){
+        List<Pet> pets = petRepo.findAll();
+        model.addAttribute("pets", pets);
+        return "PetList";
+
+    }
 }
--- s09Exe/JAVA-SPRINGBOOTMER/spring-one-to-one/src/main/java/com/t3/springonetoone/repository/PersonRepository.java
+++ s09Exe/JAVA-SPRINGBOOTMER/spring-one-to-one/src/main/java/com/t3/springonetoone/repository/PersonRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.t3.springonetoone.entity.Person;
 
 public interface PersonRepository extends JpaRepository <Person, Long>{
+
+    void save(Person person);
     
 }
