c766ef38742f65f12beb9e8097664affba728789
unknown <elisandro.santolin@outlook.com>
2023-06-01 20:21:15 -0300

    Refactor: created fetches finished items

--- s15Exe/hortifruti-app/src/App.js
+++ s15Exe/hortifruti-app/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Estoque from './components/Estoque';
 
 const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Fruits",     price: "$1", stocked: true,  name: "Apple"},
+  {category: "Fruits",     price: "$1", stocked: true,  name: "Dragonfruit"},
+  {category: "Fruits",     price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true,  name: "Spinach"},
   {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  {category: "Vegetables", price: "$1", stocked: true,  name: "Peas"},
+  {category: "Fruits",     price: "$5", stocked: true,  name: "Tomato"}
 ];
 
 function App() {
   return (
     <div>
-      <p>App</p>
       <Estoque produtos={PRODUCTS}/>
     </div>
   );
--- s15Exe/hortifruti-app/src/components/Estoque.js
+++ s15Exe/hortifruti-app/src/components/Estoque.js
@@ -1,28 +1,45 @@
 import TabelaProdutos from "./TabelaProdutos";
+import {useState} from 'react';
 
-export default function Estoque({produtos}){
+export default function Estoque({produtos}) {
+
+    const [filtro, setFiltro] = useState("");
+    const [somenteEstoque, setSomenteEstoque] = useState(false);
 
     return(
         <>
-            <p>Componentes Estoque</p>
-            <BarraPesquisa/>
-            <TabelaProdutos produtos={produtos}/>
+            <BarraPesquisa 
+                filtro={filtro} 
+                aoModificarFiltro={setFiltro}
+                somenteEstoque={somenteEstoque}
+                aoModificarEstoque={setSomenteEstoque}
+            />
+            <TabelaProdutos 
+                produtos={produtos} 
+                filtro={filtro}
+                somenteEstoque={somenteEstoque}
+            />
         </>
     );
 }
 
-function BarraPesquisa(){
+function BarraPesquisa({filtro, somenteEstoque, aoModificarFiltro, aoModificarEstoque}){
     return(
-        <form>  
-            <input 
-                type="text" 
-                placeholder="Pesquisar">
-            </input> 
-            <label>
-                <input type="checkbox"> 
-                </input>
-                    Somente mostrar produtos em estoque
-            </label>   
+        <form>
+        <input
+            type="text" 
+            placeholder="Pesquisar..."
+            value={filtro}
+            onChange={(e) => aoModificarFiltro(e.target.value)}
+        />
+        <label>
+            <input
+                type="checkbox"
+                checked={somenteEstoque}
+                onChange={(e) => aoModificarEstoque(e.target.checked)}
+            />
+            Somente mostrar produtos em estoque
+        </label>
         </form>
     );
 }
